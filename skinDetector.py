fa9c29637de4032b4cb2098410df0ca80c58366e
Andres Barro <andresbarroe+git@gmail.com>
2019-10-20 19:48:15 -0500

    working
    Signed-off-by: Andres Barro <andresbarroe+git@gmail.com>

--- skinDetector.py
+++ skinDetector.py
@@ -29,7 +29,7 @@ def detect_skin(image):
     RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     lower_HSV_values = np.array([0, 40, 0], dtype="uint8")
-    upper_HSV_values = np.array([25, 255, 255], dtype="uint8")
+    upper_HSV_values = np.array([25, 105, 255], dtype="uint8")
 
     lower_YCbCr_values = np.array((0, 138, 67), dtype="uint8")
     upper_YCbCr_values = np.array((255, 173, 133), dtype="uint8")
@@ -45,8 +45,11 @@ def detect_skin(image):
     mask_RGB = cv2.inRange(
         RGB_image, lower_RGB_values, upper_RGB_values)
 
+    # binary_mask_image = cv2.add(cv2.add(mask_HSV, mask_YCbCr), mask_RGB)
+
     binary_mask_image = cv2.add(mask_HSV, mask_YCbCr)
-    binary_mask_image = cv2.bitwise_and(binary_mask_image, binary_mask_image, mask=mask_RGB)
+    # binary_mask_image = cv2.bitwise_and(
+    #     binary_mask_image, binary_mask_image, mask=mask_RGB)
     cv2.imshow("original_binary_image", binary_mask_image)
     binary_mask_image = cv2.GaussianBlur(binary_mask_image, (11, 11), 0)
     _, binary_mask_image = cv2.threshold(
@@ -55,8 +58,7 @@ def detect_skin(image):
 
     kernel = np.ones((3, 3), np.uint8)
     image_foreground = cv2.erode(
-        binary_mask_image, kernel, iterations=1)  # remove noise
-    cv2.imshow("erosion", image_foreground)
+        binary_mask_image, kernel, iterations=3)  # remove noise
     # The background region is reduced a little because of the dilate operation
     dilated_binary_image = cv2.dilate(
         binary_mask_image, kernel, iterations=3)
@@ -76,24 +78,53 @@ def detect_skin(image):
     return cv2.bitwise_and(image, image, mask=image_mask)
 
 
+def foreground_mask(bg, image, threshold=25):
+    # find the absolute difference between background and current frame
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+    diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+    # threshold the diff image so that we get the foreground
+    thresholded = cv2.threshold(
+        diff, threshold, 255, cv2.THRESH_BINARY)[1]
+
+    opening = cv2.morphologyEx(
+        thresholded, cv2.MORPH_DILATE, cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15)), iterations=3)
+
+    return opening
+
+
+def processFrame(frame):
+    frame = cv2.flip(frame, 1)
+    # frame = cv2.GaussianBlur(frame, (3, 3), 3)
+    # frame = adjust_gamma(frame, gamma=2)
+    return frame
+
+
 if __name__ == "__main__":
     # get the reference to the webcam
     camera = cv2.VideoCapture(0)
+    _, frame = camera.read()
+    frame = processFrame(frame)
+    background = frame.copy().astype("float")
+    for _ in range(30):
+        _, frame = camera.read()
+        frame = processFrame(frame)
+        # compute weighted average, accumulate it and update the background
+        cv2.accumulateWeighted(frame, background, 0.5)
     while True:
         ret, frame = camera.read()  # BGR
-        frame = cv2.flip(frame, 1)
+        # frame = cv2.flip(frame, 1)
         cv2.imshow("initial", frame)
+        frame = processFrame(frame)
+        foreground = foreground_mask(background, frame)
 
-        blur = cv2.GaussianBlur(frame, (3, 3), 0)
-        cv2.imshow("blur", blur)
-
-        enhanced = adjust_gamma(blur, gamma=2)
-        cv2.imshow("enhanced", enhanced)
-
-        frame = enhanced
         result = detect_skin(frame)
-        # show img
         cv2.imshow("result", result)
+
+        result = cv2.bitwise_and(result, result, mask=foreground)
+
+        # show img
+        cv2.imshow("result foreground   ", result)
         # observe the keypress by the user
         keypress = cv2.waitKey(1) & 0xFF
         # if the user pressed "q", then stop looping
